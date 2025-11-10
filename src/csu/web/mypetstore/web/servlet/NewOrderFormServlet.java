79315bfc774faec37fbad0c245788d0ba6592f06
Stephanie <18773248033@163.com>
2025-11-10 16:01:52 +0800

    就这些

--- src/csu/web/mypetstore/web/servlet/NewOrderFormServlet.java
+++ src/csu/web/mypetstore/web/servlet/NewOrderFormServlet.java
@@ -1,6 +1,8 @@
 package csu.web.mypetstore.web.servlet;
 
 import csu.web.mypetstore.domain.Account;
+import csu.web.mypetstore.domain.Cart;
+import csu.web.mypetstore.domain.Order;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,9 +19,14 @@ public class NewOrderFormServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
         Account loginAccount = (Account) session.getAttribute("loginAccount");
+        Cart cart = (Cart) session.getAttribute("cart");
         if(loginAccount == null){
-            resp.sendRedirect("signOnForm");
+            resp.sendRedirect("signonForm");
         }else {
+            // ✅ 创建订单对象并保存到 session （关键步骤）
+            Order order = new Order();
+            order.initOrder(loginAccount, cart);
+            session.setAttribute("order", order);
             req.getRequestDispatcher(NEW_ORDER_FORM).forward(req, resp);
         }
     }
